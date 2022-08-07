9ecf7b2d84ca2774fdd7b8e14bb829b747524151
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2022-08-07 20:13:09 +0100

    Replace get with getAll

---
+++ etl-rules/getAll.ts
@@ -0,0 +1,19 @@
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { ddbClient, handleError, Config } from "./util";
+import { withDefaultMiddy } from "./middleware";
+
+const lambdaHandler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  const params = new ScanCommand({ TableName: Config.TABLE_NAME });
+  const data = await ddbClient.send(params);
+  const formattedItems = data?.Items?.map((item) => unmarshall(item)) || [];
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ Count: data.Count, Items: formattedItems }),
+  };
+};
+
+export const handler = withDefaultMiddy(lambdaHandler);
