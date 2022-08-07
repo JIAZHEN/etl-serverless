52e344f746b845999701588c60563c31d1e99617
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2022-08-07 22:29:24 +0100

    Create getOne

---
+++ etl-rules/getOne.ts
@@ -0,0 +1,32 @@
+import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { ddbClient, Config } from "./util";
+import { withDefaultMiddy } from "./middleware";
+import { UnprocessableEntity, NotFound } from "http-errors";
+
+const lambdaHandler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  if (!event?.pathParameters?.id) {
+    throw new UnprocessableEntity();
+  }
+
+  const params = new GetItemCommand({
+    TableName: Config.TABLE_NAME,
+    Key: { uuid: { S: event.pathParameters.id } },
+  });
+
+  const data = await ddbClient.send(params);
+
+  if (!data?.Item) {
+    throw new NotFound(`Rule not found with ID ${event.pathParameters.id}`);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(unmarshall(data.Item)),
+  };
+};
+
+export const handler = withDefaultMiddy(lambdaHandler);
