dcd008841d2baae89620307cb1543b6c3fb8d271
Jiazhen Xie <jiazhen.xie@gousto.co.uk>
2022-08-28 10:54:01 +0100

    Integrate Auth0 with authProvider

--- etl-ui/src/admin/index.tsx
+++ etl-ui/src/admin/index.tsx
@@ -4,25 +4,43 @@ import { RuleList, RuleEdit, RuleCreate } from "./Rules";
 import { RecordList, RecordEdit, RecordCreate, RecordShow } from "./Records";
 import RuleFolderIcon from "@mui/icons-material/RuleFolder";
 import PublishedWithChangesIcon from "@mui/icons-material/PublishedWithChanges";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const AdminApp = () => (
-  <Admin dataProvider={dataProviders}>
-    <Resource
-      name="etl-rules"
-      list={RuleList}
-      edit={RuleEdit}
-      create={RuleCreate}
-      icon={RuleFolderIcon}
-    />
-    <Resource
-      name="etl-records"
-      list={RecordList}
-      edit={RecordEdit}
-      create={RecordCreate}
-      show={RecordShow}
-      icon={PublishedWithChangesIcon}
-    />
-  </Admin>
-);
+const AdminApp = () => {
+  const { isAuthenticated, logout, loginWithRedirect, user } = useAuth0();
 
+  const authProvider = {
+    login: loginWithRedirect,
+    logout: () => Promise.resolve(logout({ returnTo: window.location.origin })),
+    checkError: () => Promise.resolve(),
+    checkAuth: () => (isAuthenticated ? Promise.resolve() : Promise.reject()),
+    getPermissions: () => Promise.reject("Unknown method"),
+    getIdentity: () =>
+      Promise.resolve({
+        id: user?.id,
+        fullName: user?.given_name,
+        avatar: user?.picture,
+      }),
+  };
+
+  return (
+    <Admin dataProvider={dataProviders} authProvider={authProvider}>
+      <Resource
+        name="etl-rules"
+        list={RuleList}
+        edit={RuleEdit}
+        create={RuleCreate}
+        icon={RuleFolderIcon}
+      />
+      <Resource
+        name="etl-records"
+        list={RecordList}
+        edit={RecordEdit}
+        create={RecordCreate}
+        show={RecordShow}
+        icon={PublishedWithChangesIcon}
+      />
+    </Admin>
+  );
+};
 export default AdminApp;
